1fbcbcf7be7bb4c789cd82d650361f1e7314d729
Terryems <emmanuelsimona@gmail.com>
2023-12-15 04:41:34 +0100

    new

---
+++ process_form.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $comment = $_POST["comment"];
+
+    $to = "emmanuelsimona@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission";
+    $message = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $comment";
+
+    // Additional headers
+    $headers = "From: $email";
+
+    // Send email
+    mail($to, $subject, $message, $headers);
+
+    // You can also send a response back to the client
+    echo "Email sent successfully!";
+} else {
+    // Redirect or handle the error as needed
+    echo "Invalid request";
+}
+?>
