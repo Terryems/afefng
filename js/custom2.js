13896329e6270beffd0b2db0710c1bc83366359d
Terryems <emmanuelsimona@gmail.com>
2024-01-29 04:35:24 +0100

    new

---
+++ js/custom2.js
@@ -0,0 +1,74 @@
+// JavaScript code to update the copyright year
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright-year').textContent = currentYear;
+
+
+
+
+// modal
+
+function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+  }
+
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+
+
+
+
+    // Function to open the modal
+    function opengabModal() {
+        $('#gabModal').modal('show');
+    }
+    
+    // Enable Bootstrap tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+
+
+    // Function to open the modal
+    function openjoeModal() {
+        $('#joeModal').modal('show');
+    }
+    
+    // Enable Bootstrap tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    // Function to open the modal
+    function openemsModal() {
+        $('#emsModal').modal('show');
+    }
+    
+    // Enable Bootstrap tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    // Function to open the modal
+    function opengraceModal() {
+        $('#graceModal').modal('show');
+    }
+    
+    // Enable Bootstrap tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+
+    // Function to open the modal
+    function openJoyModal() {
+        $('#joyModal').modal('show');
+    }
+    
+    // Enable Bootstrap tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
